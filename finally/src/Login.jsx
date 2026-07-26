6bdbfd49c85b947b120920ceb6a03947e146d8bd
fred <fred>
2026-07-25 22:40:59 -0700

    all

--- finally/src/Login.jsx
+++ finally/src/Login.jsx
@@ -32,7 +32,7 @@ function Login() {
         if(data.role=="admin"){
             localStorage.setItem('name');
         }
-         else if(data.message =="incorrect credentialshfvhvgjdbkghtkhy4uyiohmdbj,wk.ljq;jIOYUKNG HYH/J;GH\/"){
+         else if(data.message =="incorrect credentials"){
             alert("incorrect credentials")
         }
         else{
