2df40ac1c59704a231e4710f7aa800ff168daafd
fred <fredtumwine11@gmail.com>
2026-08-06 11:53:18 -0700

    changes

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,130 +1,120 @@
 import React, { useEffect, useState } from 'react'
-import im1 from './assets/im1.jpg';
-import im2 from './assets/im2.jpg';
-import im3 from './assets/im3.jpg';
-import im4 from './assets/im4.jpg';
+import im1 from './assets/im1.jpg'
+import im2 from './assets/im2.jpg'
+import im3 from './assets/im3.jpg'
+import im4 from './assets/im4.jpg'
 
 function App() {
 
-const container  = [
+  const holder = [
+    {
+      image:im1,
+      title:"gutera inkunga"
+    },
+    {
+      image:im2,
+      title:"gufasha abatishoboye"
+    },
+    {
+      image:im3,
+      title:"guhemba abakozi"
+    },
+    {
+      image:im4,
+      title:"gubitsa amafaranga"
+    }
+  ];
 
-  {
-    image:im1
-  },
-  {
-    image:im2
-  },
-  {
-    image:im3
-  },
-  {
-    image:im4
+
+  const [current,setCurrent] = useState(0);
+
+  const prevSlide = ()=>{
+    setCurrent((prev)=>(prev -1 + holder.length)%holder.length);
   }
-];
 
-const slide = [
-  container.slice(0,3),
-  container.slice(1,4)
-];
-const [page,setPage] = useState(0);
+  const nextSlide = ()=>{
+    setCurrent((prev)=>(prev +1) %holder.length);
+  }
 
-const [one,setOne] = useState(0);
-
-const prevSlide = ()=>{
-
-  setOne ((prev=>(prev -1 + container.length)%container.length)
-)}
-
-const nextSlide = ()=>{
- setOne((prev=>(prev +1) %container.length));
-
-}
-useEffect(()=>{
-  const HandleC = (e)=>{
-    if(e.key == 'ArrowRight')nextSlide();
-    if(e.key == 'ArrowLeft')prevSlide();
-
-  };
-
-  window.addEventListener('keydown',HandleC)
-  return ()=> window.addEventListener('keydown',HandleC)
-
-},[]);
-useEffect(()=>{
-  const timer = setInterval(() => {
+ useEffect(()=>{
+  const timer = setInterval(()=>{
     nextSlide();
-  }, 5000);
-  return()=>clearInterval(timer);
-})
+  },5000)
+
+  return ()=>clearInterval(timer)
+ },[]);
+
+ useEffect(()=>{
+  const handleC = (e)=>{
+    if(e.key == 'ArrowRight')nextSlide()
+      if(e.key == 'ArrowLeft')prevSlide()
+  }
+
+  window.addEventListener('keydown',handleC)
+  return ()=>window.removeEventListener('keydown',handleC);
+ },[]);
+
+
+ const group = [
+  holder.slice(0,3),
+  holder.slice(1,4)
+ ];
+
+ const [page,setPage] = useState(0);
+
+useEffect(()=>{
+  const tell = setInterval(()=>{
+     setPage((prev) => (prev + 1) % group.length);
+  },5000);
+
+  return ()=> clearInterval(tell)
+},[])
+
+
   return (
-
-
     <div>
+      
+      <div className=' bg-cover bg-no-repeat bg-center min-h-screen' style={{backgroundImage:`url(${holder[current].image})`}}>
 
-
-
-       <div className='bg-cover bg-center w-full min-h-screen bg-no-repeat' style={{backgroundImage:`url(${container[one].image})`}}>
-
-
-       <button onClick={(nextSlide)} className='text-white border'>rigth</button>  
-       <button onClick={(prevSlide)}  className='text-white border'>left</button> 
-       <div className="flex justify-center gap-3 mt-5">
-
-{
-  container.map((_,index)=>(
-
-    <button
-      key={index}
-      onClick={()=>setOne(index)}
-      className={`
-        w-3 h-3 rounded-full
-        ${
-          one === index
-          ? "bg-green-600"
-          : "bg-gray-300"
-        }
-      `}
-    ></button>
-
-  ))
-}
-
-</div> 
-
-    </div>
+        <button onClick={()=>nextSlide()} className='border text-white'>RIGHT</button>
+        <button onClick={()=>prevSlide()} className='border text-white'>left</button>
+         {
+          holder.map((_,index)=>(
+            <button onClick={()=>setCurrent(index)} className={`w-4 h-4 rounded-full ${current === index ? "bg-green-400":"bg-gray-300"}`}></button>
+          ))
+         }
+      </div>
 
 
      <div className='overflow-hidden'>
-      <div className='flex min-w-full ' style={{transform:`translateX(-${page * 100}%)`}}>
+      <div className='flex transition-transform duration-500 ease-in-out' style={{transform:`translateX(-${ page * 100}%)`}}>
         {
-          slide.map((photo,index)=>(
-
-            <div key={index} className='grid grid-cols-3 min-w-full gap-4'>
+          group.map((each,index)=>(
+            <div key={index} className='min-w-full flex  gap-5'>
               {
-                photo.map((item,index)=>(
-                  <div key={index}>
-                    <img src={item.image} alt="" />
+                each.map((one,index)=>(
+                  <div key={index} className='flex-1 flex flex-col gap-2' >
+                    <img src={one.image} alt="" />
+                    <h1>{one.title}</h1>
                   </div>
                 ))
               }
-
             </div>
           ))
         }
       </div>
-     </div>
-     <div>
+        
+        <div>
       {
-        slide.map((_,index)=>(
-          <button key={index} onClick={()=>setPage(index)} className={`w-4 h-4 rounded-full ${page === index ? "bg-green-400":"bg-gray-300"}`}></button>
+        group.map((_,index)=>(
+          <button onClick={()=>setPage(index)} className={`w-4 h-4 rounded-full ${page === index ? "bg-green-400":"bg-gray-400"}`}></button>
         ))
       }
      </div>
-    
-   
-     
+</div>
+      
     </div>
-
+    
   )
 }
 
